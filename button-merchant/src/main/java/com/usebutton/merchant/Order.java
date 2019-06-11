119c2972c5a2a7a98549c56bc1c6ff1cab0c9d98
Jonathan Petit-Frere <jonathan@usebutton.com>
2019-06-11 15:09:25 -0400

    Update order and builder with new variables

--- button-merchant/src/main/java/com/usebutton/merchant/Order.java
+++ button-merchant/src/main/java/com/usebutton/merchant/Order.java
@@ -25,16 +25,30 @@
 
 package com.usebutton.merchant;
 
-import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import java.util.Date;
+import java.util.List;
 
 /**
  * Represents an order placed by the user to be tracked using ButtonMerchant.trackOrder(order).
  */
 public class Order {
-    @NonNull
+
     private String id;
+    @Deprecated
     private long amount;
     private String currencyCode;
+    @Nullable
+    private Date purchaseDate;
+    @Nullable
+    private List<LineItem> lineItems;
+    @Nullable
+    private String sourceToken;
+    @Nullable
+    private String customerOrderId;
+    @Nullable
+    private Customer customer;
 
     /**
      * Constructor.
@@ -45,13 +59,18 @@ public class Order {
         this.id = builder.id;
         this.amount = builder.amount;
         this.currencyCode = builder.currencyCode;
+        this.purchaseDate = builder.purchaseDate;
+        this.lineItems = builder.lineItems;
+        this.sourceToken = builder.sourceToken;
+        this.customerOrderId = builder.customerOrderId;
+        this.customer = builder.customer;
     }
 
-    @NonNull
     public String getId() {
         return id;
     }
 
+    @Deprecated
     public long getAmount() {
         return amount;
     }
@@ -60,21 +79,57 @@ public class Order {
         return currencyCode;
     }
 
+    @Nullable
+    public Date getPurchaseDate() {
+        return purchaseDate;
+    }
+
+    @Nullable
+    public List<LineItem> getLineItems() {
+        return lineItems;
+    }
+
+    @Nullable
+    public String getSourceToken() {
+        return sourceToken;
+    }
+
+    @Nullable
+    public String getCustomerOrderId() {
+        return customerOrderId;
+    }
+
+    @Nullable
+    public Customer getCustomer() {
+        return customer;
+    }
+
     /**
      * Builder class for tracking a customer {@link Order}.
      */
     public static class Builder {
-        @NonNull
+
         private String id;
+        @Deprecated
         private long amount = 0;
         private String currencyCode = "USD";
+        @Nullable
+        private Date purchaseDate;
+        @Nullable
+        private List<LineItem> lineItems;
+        @Nullable
+        private String sourceToken;
+        @Nullable
+        private String customerOrderId;
+        @Nullable
+        private Customer customer;
 
         /**
          * Constructor.
          *
          * @param id The order identifier (required).
          */
-        public Builder(@NonNull String id) {
+        public Builder(String id) {
             this.id = id;
         }
 
@@ -82,6 +137,7 @@ public class Order {
          * The total order value in pennies (e.g. 3999 for $39.99) or the smallest decimal unit of
          * the currency code. (default is 0)
          */
+        @Deprecated
         public Builder setAmount(long amount) {
             this.amount = amount;
             return this;
@@ -96,6 +152,46 @@ public class Order {
         }
 
         /**
+         * The time the purchase was made by the user (required)
+         */
+        public Builder setPurchaseDate(Date purchaseDate) {
+            this.purchaseDate = purchaseDate;
+            return this;
+        }
+
+        /**
+         * A list of the line item details that comprise the order (required)
+         */
+        public Builder setLineItems(List<LineItem> lineItems) {
+            this.lineItems = lineItems;
+            return this;
+        }
+
+        /**
+         * The button source token
+         */
+        public Builder setSourceToken(String sourceToken) {
+            this.sourceToken = sourceToken;
+            return this;
+        }
+
+        /**
+         * The customer-facing order id
+         */
+        public Builder setCustomerOrderId(String customerOrderId) {
+            this.customerOrderId = customerOrderId;
+            return this;
+        }
+
+        /**
+         * The customer related to the order
+         */
+        public Builder setCustomer(Customer customer) {
+            this.customer = customer;
+            return this;
+        }
+
+        /**
          * Builds and returns an Order Object
          *
          * @return {@link Order}
@@ -103,5 +199,13 @@ public class Order {
         public Order build() {
             return new Order(this);
         }
+    }
+
+    public class LineItem {
+
+    }
+
+    public class Customer {
+
     }
 }
--- button-merchant/src/test/java/com/usebutton/merchant/OrderTest.java
+++ button-merchant/src/test/java/com/usebutton/merchant/OrderTest.java
@@ -27,7 +27,13 @@ package com.usebutton.merchant;
 
 import org.junit.Test;
 
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.mockito.Mockito.mock;
 
 public class OrderTest {
 
@@ -38,23 +44,64 @@ public class OrderTest {
         assertEquals(order.getId(), "123");
         assertEquals(order.getAmount(), 0);
         assertEquals(order.getCurrencyCode(), "USD");
+        assertNull(order.getPurchaseDate());
+        assertNull(order.getLineItems());
+        assertNull(order.getSourceToken());
+        assertNull(order.getCustomerOrderId());
+        assertNull(order.getCustomer());
     }
 
     @Test
     public void shouldSetAmount() {
         Order order = new Order.Builder("123").setAmount(1000).build();
 
-        assertEquals(order.getId(), "123");
         assertEquals(order.getAmount(), 1000);
-        assertEquals(order.getCurrencyCode(), "USD");
     }
 
     @Test
     public void shouldSetCurrencyCode() {
         Order order = new Order.Builder("123").setCurrencyCode("AUG").build();
 
-        assertEquals(order.getId(), "123");
-        assertEquals(order.getAmount(), 0);
         assertEquals(order.getCurrencyCode(), "AUG");
+    }
+
+    @Test
+    public void shouldSetPurchaseDate() {
+        Date purchaseDate = new Date();
+        Order order = new Order.Builder("123").setPurchaseDate(purchaseDate).build();
+
+        assertEquals(order.getPurchaseDate(), purchaseDate);
+    }
+
+    @Test
+    public void shouldSetLineItems() {
+        List<Order.LineItem> lineItems = Collections.emptyList();
+        Order order = new Order.Builder("123").setLineItems(lineItems).build();
+
+        assertEquals(order.getLineItems(), lineItems);
+    }
+
+    @Test
+    public void shouldSetSourceToken() {
+        String sourceToken = "valid_source_token";
+        Order order = new Order.Builder("123").setSourceToken(sourceToken).build();
+
+        assertEquals(order.getSourceToken(), sourceToken);
+    }
+
+    @Test
+    public void shouldSetCustomerOrderId() {
+        String customerOrderId = "valid_customer_order_id";
+        Order order = new Order.Builder("123").setCustomerOrderId(customerOrderId).build();
+
+        assertEquals(order.getCustomerOrderId(), customerOrderId);
+    }
+
+    @Test
+    public void shouldSetCustomer() {
+        Order.Customer customer = mock(Order.Customer.class);
+        Order order = new Order.Builder("123").setCustomer(customer).build();
+
+        assertEquals(order.getCustomer(), customer);
     }
 }
