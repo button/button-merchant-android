fde43e656fc1ebbcf6b7b67f8905dab42d629f0c
Wes Smith <wes@usebutton.com>
2022-10-25 10:07:26 -0400

    Generate v1.5.0 docs

---
+++ docs/history/1.5.0/reference/lists.js
@@ -0,0 +1,26 @@
+var DATA = [
+      { id:0, label:"com.usebutton.merchant", link:"com/usebutton/merchant/package-summary.html", type:"package" },
+      { id:1, label:"com.usebutton.merchant.ActivityReportingTask", link:"com/usebutton/merchant/ActivityReportingTask.html", type:"class" },
+      { id:2, label:"com.usebutton.merchant.ButtonMerchant", link:"com/usebutton/merchant/ButtonMerchant.html", type:"class" },
+      { id:3, label:"com.usebutton.merchant.ButtonMerchant.AttributionTokenListener", link:"com/usebutton/merchant/ButtonMerchant.AttributionTokenListener.html", type:"class" },
+      { id:4, label:"com.usebutton.merchant.ButtonProduct", link:"com/usebutton/merchant/ButtonProduct.html", type:"class" },
+      { id:5, label:"com.usebutton.merchant.ButtonProductCompatible", link:"com/usebutton/merchant/ButtonProductCompatible.html", type:"class" },
+      { id:6, label:"com.usebutton.merchant.Order", link:"com/usebutton/merchant/Order.html", type:"class" },
+      { id:7, label:"com.usebutton.merchant.Order.Builder", link:"com/usebutton/merchant/Order.Builder.html", type:"class" },
+      { id:8, label:"com.usebutton.merchant.Order.Customer", link:"com/usebutton/merchant/Order.Customer.html", type:"class" },
+      { id:9, label:"com.usebutton.merchant.Order.Customer.Builder", link:"com/usebutton/merchant/Order.Customer.Builder.html", type:"class" },
+      { id:10, label:"com.usebutton.merchant.Order.LineItem", link:"com/usebutton/merchant/Order.LineItem.html", type:"class" },
+      { id:11, label:"com.usebutton.merchant.Order.LineItem.Builder", link:"com/usebutton/merchant/Order.LineItem.Builder.html", type:"class" },
+      { id:12, label:"com.usebutton.merchant.OrderListener", link:"com/usebutton/merchant/OrderListener.html", type:"class" },
+      { id:13, label:"com.usebutton.merchant.PostInstallIntentListener", link:"com/usebutton/merchant/PostInstallIntentListener.html", type:"class" },
+      { id:14, label:"com.usebutton.merchant.UserActivityListener", link:"com/usebutton/merchant/UserActivityListener.html", type:"class" },
+      { id:15, label:"com.usebutton.merchant.exception", link:"com/usebutton/merchant/exception/package-summary.html", type:"package" },
+      { id:16, label:"com.usebutton.merchant.exception.ApplicationIdNotFoundException", link:"com/usebutton/merchant/exception/ApplicationIdNotFoundException.html", type:"class" },
+      { id:17, label:"com.usebutton.merchant.exception.ButtonNetworkException", link:"com/usebutton/merchant/exception/ButtonNetworkException.html", type:"class" },
+      { id:18, label:"com.usebutton.merchant.exception.HttpStatusException", link:"com/usebutton/merchant/exception/HttpStatusException.html", type:"class" },
+      { id:19, label:"com.usebutton.merchant.exception.NetworkNotFoundException", link:"com/usebutton/merchant/exception/NetworkNotFoundException.html", type:"class" },
+      { id:20, label:"com.usebutton.merchant.module", link:"com/usebutton/merchant/module/package-summary.html", type:"package" },
+      { id:21, label:"com.usebutton.merchant.module.ButtonUserActivity", link:"com/usebutton/merchant/module/ButtonUserActivity.html", type:"class" },
+      { id:22, label:"com.usebutton.merchant.module.Features", link:"com/usebutton/merchant/module/Features.html", type:"class" }
+
+    ];
